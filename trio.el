2cbfc43d7e6c497bab4a70725962f9f6c51b7efa
chimay <orduval@gmail.com>
2019-03-23 12:51:46 +0100

    structure

---
+++ trio.el
@@ -0,0 +1,41 @@
+;;; duo.el --- In place list operations in Elisp     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019 Chimay
+
+;; Author : Chimay
+;; Name: Duo
+;; Package-Version: 1.0
+;; Package-requires: ((emacs "26"))
+;; Keywords: lisp, extensions, list, in-place, operation
+;; URL: https://github.com/chimay/duo
+
+;;; Commentary:
+
+;; trio = (CAR . CDR)
+;;
+;; car trio = reference to the first cons in list
+;; cdr trio = reference to the last cons in list
+;;
+;; car of reference to cons in list = reference to previous cons in list
+;; cdr of reference to cons in list = cons in list
+
+
+;;; License:
+;;; ----------------------------------------------------------------------
+
+;; This file is not part of Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING. If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
